8dac8aa29f3e503675be5a8e85575d3de2f7be0f
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-04-27 19:48:12 -0600

    class apr 27

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // I can comment about this! My opinion matters!
+        // Console.WriteLine("Hello Sandbox World!");
+
+        // bool yes = false;
+        // do
+        // {  
+        //     //always executes at least once.
+        //     Console.WriteLine("Something");
+        // } while (yes);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Something {i}");
+        }
     }
 }
