4b2a1e81a8ae54efc0e6fcbcc55eb49413342df1
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-04-27 20:24:50 -0600

    did prep 3 apr 27

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        bool playing = false;
+
+        do //play the game
+        {
+            // create magic number
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,100);
+
+            // Console.Write("What is the magic number? ");
+            // string strNumber = Console.ReadLine();
+            // int number = int.Parse(strNumber);
+
+            int num;
+            int counter = 0;
+            do //guesses
+            {
+                // ask for user's number
+                Console.Write("What is your guess? ");
+                string strNum = Console.ReadLine();
+                num = int.Parse(strNum);
+
+                // evaluate user's number
+                if (num > number)
+                {
+                    Console.WriteLine("Lower");
+                    counter = counter + 1;
+                }
+                else if (num < number)
+                {
+                    Console.WriteLine("Higher");
+                    counter = counter + 1;
+                }
+                else
+                {
+                    Console.WriteLine("You Guessed it!");
+                    counter = counter + 1;
+                }
+            } while (num != number);
+
+            Console.WriteLine($"You took {counter} guesses!");
+
+            // ask to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string askPlaying = Console.ReadLine();
+
+            if (askPlaying == "yes" || askPlaying == "Yes" || askPlaying == "YES")
+            {
+                playing = true;
+            }
+            else
+            {
+                playing = false;
+            }
+                
+        } while (playing == true);
     }
 }
