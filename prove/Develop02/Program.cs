2cffb6c9d57f50adbdb5588dbc5128aea5694def
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-05-16 22:56:33 -0600

    comments for exceeding requirements

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,27 +13,30 @@ class Program
         // make loop that repeats until quit
         do
         {
-        // make options to choose
+            // make options to choose
             // Write Display load save quit
             Console.WriteLine("\nPlease select one of the following choices:");
             Console.WriteLine("1. Write \n2. Display Journal \n3. Load \n4. Save \n5. Quit");
-            Console.Write("What would you like to do? (use #) ");
+            Console.Write("What # would you like to do? ");
             int answer = int.Parse(Console.ReadLine());
 
             // evaluate choice
             if (answer == 1)
             {
+                // adds a single entry
                 journal.AddEntry();
             }
 
             else if (answer == 2)
             {
-                // Display journal
+                // Display full journal and entries
                 journal.DisplayEntries();
             }
 
             else if (answer == 3)
             {
+                // exceeds requirements to have specific senarios of saving and loading. 
+                // It is practically an autosave.
                 if (journal._saved == true)
                 {
                     // Load journal without saving it because it's already saved
@@ -51,6 +54,8 @@ class Program
 
             else if (answer == 4)
             {
+                // exceeds requirements with load and save senarios, 
+                // but the code is in journal.cs
                 // Save journal
                 journal.Save();
                 // Console.WriteLine("Journal is totally saved");
@@ -65,3 +70,6 @@ class Program
         } while (journalling == true);
     }
 }
+// load and save senarios exceeds requirements, 
+// more information found throughout the code.
+// ctrl + f to find 'exceeds requirements'
