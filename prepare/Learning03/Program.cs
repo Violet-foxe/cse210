35e96679dcc977f4af7e0447263157fd0fbc15ba
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-05-18 00:16:05 -0600

    made the fraction calculator thing may 18

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,25 +2,38 @@ using System;
 
 class Program
 {
-    public static double FindSmallest(List<double> list )
-    {
-        double smallest = list[0];
-        for (int i = 0; i < list.Count(); i++)
-        {
-            if (smallest > list[i])
-            {
-                smallest = list[i];
-            }
-        }
-        return smallest;
-    }
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Learning03 World!");
-        List<double> sprintTimes = new List<double> {9.58, 9.69, 9.72, 9.74, 9.78};
-        Console.WriteLine("100 Meter Dash Fastest Time Lookup");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
 
-        double shortest = FindSmallest(sprintTimes);
-        Console.WriteLine($"{shortest} seconds");
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 7);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        
+        Fraction fraction = new Fraction();
+        Random rnd = new Random();
+
+        for (int i=0; i<30; i++) 
+        {
+            int top = rnd.Next(1,13);
+            int bottom = rnd.Next(1,13);
+
+            fraction.SetTop(top);
+            fraction.SetBottom(bottom);
+
+            Console.Write($"String {i+1}: {fraction.GetFractionString()}, ");
+            Console.WriteLine($"Decimal {i+1}: {fraction.GetDecimalValue()}\n");
+        }
     }
 }
