a85f7f27d3e89af7a1531e95d978ab38e3eba0df
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-06-22 06:42:02 -0600

    shape learning program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        // Console.WriteLine("Hello Learning05 World!");
+
+        Square sh1 = new Square("red", 2);
+        Rectangle sh2 = new Rectangle("blue", 2, 3);
+        Circle sh3 = new Circle("yellow", 1);
+        
+        List<Shape> shapes = new();
+        shapes.Add(sh1);
+        shapes.Add(sh2);
+        shapes.Add(sh3);
+
+        foreach (Shape sh in shapes)
+        {
+            Console.WriteLine($"{sh.GetColor()}, a: {sh.GetArea()}");
+        }
+
+        Console.WriteLine();
     }
 }
